189f76db85a3eb0ef504453891712be922cf1c12
tysudess <rodrigodess@yahoo.com.br>
2026-09-24 12:31:22 -0300

    Bump Android version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.centralmidia.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
